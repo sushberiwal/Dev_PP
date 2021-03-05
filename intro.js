fa5dc4cd6030c6df0fcab0ff35308c70347e805b
Sushant <sushantberiwal@gmail.com>
2021-03-05 20:46:40 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,9 @@
+// cout << "";
+// System.out.println("");
+// node js not installed => cannot run .js files
+// JS file => V8 engine
+
+// top to down
+// left to right
+
+console.log("hello world !");
