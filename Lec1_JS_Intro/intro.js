1c0be8c8994af2bf17fe89f3ad2d55e1e4e10923
Sushant <sushantberiwal@gmail.com>
2021-03-06 19:09:02 +0530

    reference links added

--- Lec1_JS_Intro/intro.js
+++ Lec1_JS_Intro/intro.js
@@ -71,6 +71,12 @@ let data = {
   }
 };
 
+// {
+//     name:"Captain America",
+//     rating:10
+// }
+
+
 // access object keys
 // dot notation => literal check
 // console.log(data.name);
@@ -94,6 +100,7 @@ data.name = "I am a new Value";
 // int arr[] = [10,20,30,50];
 // int arr[] = new int[10];
 
+// array => pop , push , shift , unshift , slice
 let values = [
   10,
 
@@ -114,4 +121,8 @@ let values = [
 // in loop
 for( let key in data ){
     console.log(key);
+}
+
+for(let i=0 ; i<100 ; i++){
+    console.log(i);
 }
---
+++ reference.md
@@ -0,0 +1,4 @@
+# Important Links
+
+* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/
+* https://javascript.info/
