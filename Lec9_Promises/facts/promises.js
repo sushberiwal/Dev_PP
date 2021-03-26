ebdabbb559d20e162f51e40cce76b0c45d6a28ae
Sushant <sushantberiwal@gmail.com>
2021-03-26 18:02:05 +0530

    promises intro

---
+++ Lec9_Promises/facts/promises.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+// B gets a pending promise          // A -> initially returns a pending promise
+let pendingPromise = fs.promises.readFile("./f2.txt"); // 100gb => 10 min
+console.log(pendingPromise);
+// pendingPromise => Promise object whose state is pending !!!
+
+// success callback => scb attached to pending promise
+pendingPromise.then(function(data){
+    console.log("Inside then ka callback i.e scb");
+    console.log(data);
+    console.log(pendingPromise);
+});
+
+
+// failure callback => fcb attached to pending promise
+pendingPromise.catch( function(error){
+    console.log("Inside catch ka callback i.e fcb");
+    console.log(error);
+    console.log(pendingPromise);
+});
---
+++ obj.js
@@ -0,0 +1,16 @@
+// let obj ={
+//     "name":"Steve",
+//     "place":"Queens",
+//     "skills":"fighting skills"
+// }
+
+// let idx=0;
+// for(key in obj){
+//     idx++;
+//     if(idx == 1){
+        
+//     }
+// }
+
+let input = process.argv.slice(2)[0];
+console.log(input);
