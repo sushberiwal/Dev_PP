c0f44cd122ec24350b6643ee5300e52160331758
Sushant <sushantberiwal@gmail.com>
2021-03-26 20:58:11 +0530

    logged in

--- Lec9_Promises/activity/codeSubmission.js
+++ Lec9_Promises/activity/codeSubmission.js
@@ -1,33 +1,49 @@
-const puppeteer = require('puppeteer');
-
+const puppeteer = require("puppeteer");
+const id = "savob68663@bombaya.com";
+const pw = "123456789";
+let tab;
 // all functions of puppeteer promisifed => gives you a pending promise initially
-
 
 // opens a new browser instance
 
-let browserOpenPromise = puppeteer.launch({ headless: false });
+let browserOpenPromise = puppeteer.launch({
+  headless: false,
+  defaultViewport: null,
+  args: ["--start-maximized"],
+  slowMo : 100
+});
 // Promise<Pending>
 
-browserOpenPromise.then(function(browser){
+browserOpenPromise
+  .then(function (browser) {
     console.log("Browser opened !!!");
-    console.log(browser);
     let allPagesPromise = browser.pages();
     return allPagesPromise;
-})
-.then(function(pages){
-    let tab = pages[0];
-    let pageOpenPromise = tab.goto("https://www.google.com");
+  })
+  .then(function (pages) {
+    tab = pages[0];
+    let pageOpenPromise = tab.goto("https://www.hackerrank.com/auth/login");
     return pageOpenPromise;
     //Promise<pending>
-})
-.then(function(){
-    console.log("google homepage opened !!!!!")
-})
-.catch(function(error){
+  })
+  .then(function () {
+      let idTypePromise = tab.type("#input-1" , id);
+      return idTypePromise;
+  })
+  .then(function(){
+      let pwTypePromise = tab.type("#input-2" , pw);
+      return pwTypePromise;
+  })
+  .then(function(){
+      let loginPromise = tab.click('.ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled');
+      return loginPromise;
+  })
+  .then(function(){
+      console.log("logged in to hackerrank !!!");
+  })
+  .catch(function (error) {
     console.log(error);
-})
-
-
+  });
 
 // const browser = await puppeteer.launch();
 // const page = await browser.newPage();
