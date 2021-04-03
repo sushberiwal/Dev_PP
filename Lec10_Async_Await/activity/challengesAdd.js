f352b4e162be47c0a3c48b5295166e387ef584c3
Sushant <sushantberiwal@gmail.com>
2021-04-03 19:06:11 +0530

    challenges Added

--- Lec10_Async_Await/activity/challengesAdd.js
+++ Lec10_Async_Await/activity/challengesAdd.js
@@ -21,6 +21,8 @@ let challenges = require("./challenges");
     await tab.click('div[data-analytics="NavBarProfileDropDown"]');
     await tab.waitForSelector('a[data-analytics="NavBarProfileDropDownAdministration"]' , {visible:true});
     await tab.click('a[data-analytics="NavBarProfileDropDownAdministration"]');
+    // delay
+    await tab.waitForTimeout(5000);
     await tab.waitForSelector('.nav-tabs.nav.admin-tabbed-nav a' , {visible:true});
     let bothATags = await tab.$$('.nav-tabs.nav.admin-tabbed-nav a');
     let manageChallengeATag = bothATags[1];
@@ -40,25 +42,46 @@ let challenges = require("./challenges");
     // OR
 
     // add challenges one by one
-    // for(let i=0 ; i<challenges.length ; i++){
-    //     // add a single challenge
-    //     await addChallenge(challenges[i] , browser , createChallengeLink );
-    // }
-
-
-    await addChallenge(challenges[0] , browser , createChallengeLink);
+    for(let i=0 ; i<challenges.length ; i++){
+        // add a single challenge
+        await addChallenge(challenges[i] , browser , createChallengeLink );
+    }
+    // await addChallenge(challenges[0] , browser , createChallengeLink);
 })();   
 
 
 
 // by default returns a pending promise
 async function addChallenge(challenge , browser , createChallengeLink ){
-
     // {
-        // 
-    // }
-    let newTab = await browser.newPage();
-    newTab.goto(createChallengeLink);
+    //     "Challenge Name": "Pep_Java_1GettingStarted_1IsPrime",
+    //     "Description": "Question 1",
+    //     "Problem Statement": "Take as input a number n. Determine whether it is prime or not. If it is prime, print 'Prime' otherwise print 'Not Prime.",
+    //     "Input Format": "Integer",
+    //     "Constraints": "n <= 10 ^ 9",
+    //     "Output Format": "String",
+    //     "Tags": ["Basics" , "Array" , "Getting_Started"]
+    //   }
 
-    newTab.close();
+    let challengeName = challenge["Challenge Name"];
+    let description = challenge["Description"];
+    let probStatement = challenge["Problem Statement"];
+    let inputFormat = challenge["Input Format"];
+    let constraints = challenge["Constraints"];
+    let outputFormat = challenge["Output Format"];
+    let tags = challenge["Tags"];
+
+    let newTab = await browser.newPage();
+    await newTab.goto(createChallengeLink);
+    await newTab.waitForSelector('#name' , {visible:true});
+    await newTab.type('#name' , challengeName );
+    await newTab.type('#preview' , description);
+    await newTab.type('#problem_statement-container .CodeMirror textarea' , probStatement );
+    await newTab.type('#input_format-container .CodeMirror textarea' , inputFormat);
+    await newTab.type('#constraints-container .CodeMirror textarea' , constraints);
+    await newTab.type('#output_format-container .CodeMirror textarea' , outputFormat);
+    await newTab.type('#tags_tag' , tags);
+    await newTab.keyboard.press("Enter");
+    await newTab.click('.save-challenge.btn.btn-green');
+    await newTab.close();
 }
