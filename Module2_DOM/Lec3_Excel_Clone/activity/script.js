0d5670da2083d5da392732f56ec1d188dd68b260
Sushant <sushantberiwal@gmail.com>
2021-04-24 19:25:22 +0530

    layout of cells complete

---
+++ Module2_DOM/Lec3_Excel_Clone/activity/script.js
@@ -0,0 +1,22 @@
+let cellsContent = document.querySelector(".cells-content");
+
+function initCells(){
+    // <div class="row">
+    // <div class="cell"></div>
+    // <div class="cell"></div>
+    // <div class="cell"></div>
+    // <div class="cell"></div>
+    // <div class="cell"></div>
+    // <div class="cell"></div>
+    // </div>
+    let cells = "";
+    for(let i=0 ; i<100 ; i++){
+        cells += "<div class='row'>"
+        for(let j=0 ; j<26 ; j++){
+            cells += "<div class='cell'>Cell</div>"
+        }
+        cells += "</div>"
+    }
+    cellsContent.innerHTML = cells;
+}
+initCells();
