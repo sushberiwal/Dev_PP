3f356aa87d8385a2dfb06429b5bac983f8b09927
Sushant <sushantberiwal@gmail.com>
2021-05-16 18:57:23 +0530

    css fixes 2

--- Module2_DOM/Lec3_Excel_Clone/activity/script.js
+++ Module2_DOM/Lec3_Excel_Clone/activity/script.js
@@ -20,34 +20,40 @@ let colId;
 
 for (let i = 0; i < allCells.length; i++) {
   allCells[i].addEventListener("click", function (e) {
+    // add active class
+    if (lastSelectedCell) {
+      lastSelectedCell.classList.remove("active-cell");
+      document
+        .querySelector(`div[trid="${colId}"]`)
+        .classList.remove("cell-selected");
+      document
+        .querySelector(`div[lcid="${rowId}"]`)
+        .classList.remove("cell-selected");
+    }
     rowId = Number(e.target.getAttribute("rowid"));
     colId = Number(e.target.getAttribute("colid"));
+    e.target.classList.add("active-cell");
+    document.querySelector(`div[trid="${colId}"]`).classList.add("cell-selected");
+    document.querySelector(`div[lcid="${rowId}"]`).classList.add("cell-selected");
+
     let cellObject = db[rowId][colId];
     let address = String.fromCharCode(65 + colId) + (rowId + 1) + "";
     addressInput.value = address;
     formulaInput.value = cellObject.formula;
 
-
-    // add active class
-    if(lastSelectedCell){
-      lastSelectedCell.classList.remove("active-cell");
-    }
-    e.target.classList.add("active-cell");
-
-
-
     cellObject.fontStyle.bold
       ? document.querySelector(".bold").classList.add("active-font-style")
       : document.querySelector(".bold").classList.remove("active-font-style");
 
-      cellObject.fontStyle.italic
+    cellObject.fontStyle.italic
       ? document.querySelector(".italic").classList.add("active-font-style")
       : document.querySelector(".italic").classList.remove("active-font-style");
 
-      cellObject.fontStyle.underline
+    cellObject.fontStyle.underline
       ? document.querySelector(".underline").classList.add("active-font-style")
-      : document.querySelector(".underline").classList.remove("active-font-style");
-
+      : document
+          .querySelector(".underline")
+          .classList.remove("active-font-style");
   });
 
   allCells[i].addEventListener("blur", function (e) {
