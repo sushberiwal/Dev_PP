30f4f898f7373b28f953431a3e9a49be5777cc7f
sushant <sushantberiwal@gmail.com>
2021-05-21 19:37:30 +0530

    mediaRecorder

--- Module2_DOM/Lec5_PepSnap/activity/script.js
+++ Module2_DOM/Lec5_PepSnap/activity/script.js
@@ -1,13 +1,52 @@
 let videoPlayer = document.querySelector("video");
+let recordButton = document.querySelector("#record-video");
+let photoButton = document.querySelector("#capture-photo");
+let recordingState = false;
 let constraints = {video:true};
+let recordedData;
+let mediaRecorder;
 
 (async function(){
-    // let devices = await navigator.mediaDevices.enumerateDevices();
-    // console.log(devices);
-
-
-    let mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
-    console.log(mediaStream);
-
-    videoPlayer.srcObject = mediaStream;    
+    try{
+        let mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+        videoPlayer.srcObject = mediaStream; 
+        mediaRecorder = new MediaRecorder(mediaStream);
+        // so next we have attached functions to these events
+        mediaRecorder.onstop = function(e){
+            console.log("Inside on stop !!");
+            console.log(e);
+        }
+        mediaRecorder.onstart = function(e){
+            console.log("Inside on start !!");
+            console.log(e);
+        }
+        mediaRecorder.ondataavailable = function(e){
+            console.log("Inside on data available !!");
+            recordedData = e.data;
+        }
+        console.log(mediaRecorder);
+    
+    
+        // attach click event on recordButton
+        recordButton.addEventListener("click" , function(){
+            if(recordingState){
+                // stop the recording
+                mediaRecorder.stop();
+                recordButton.innerHTML = "Record";
+            }
+            else{
+                //start the recording
+                mediaRecorder.start();
+                recordButton.innerHTML = "Recording";
+            }
+            recordingState = !recordingState;
+        })
+    }
+    catch(error){
+    }
 })();
+
+
+
+
+
