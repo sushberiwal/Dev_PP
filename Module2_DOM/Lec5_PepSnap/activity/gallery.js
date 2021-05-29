66ab4de23ced88d9bc72ee5d746d175c0599071f
sushant <sushantberiwal@gmail.com>
2021-05-29 19:41:45 +0530

    added gallery page !

---
+++ Module2_DOM/Lec5_PepSnap/activity/gallery.js
@@ -0,0 +1,21 @@
+function showMedia(){
+    // assume db is open !!
+    let txn = db.transaction("Media" , "readonly");
+    let mediaStore = txn.objectStore("Media");
+    let cursorObject = mediaStore.openCursor();
+
+    cursorObject.onsuccess = function(e){
+        let cursor = cursorObject.result;
+        if(cursor){
+            console.log(cursor.value);
+            cursor.continue();
+        }
+    }
+}
+
+let iv = setInterval( function(){
+    if(db){
+        showMedia();
+        clearInterval(iv);
+    }
+}   , 100);
