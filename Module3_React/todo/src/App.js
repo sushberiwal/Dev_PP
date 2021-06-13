36479394cc9399aa04f0592cbd5088cfea52d5af
sushant <sushantberiwal@gmail.com>
2021-06-13 19:55:40 +0530

    created two components

--- Module3_React/todo/src/App.js
+++ Module3_React/todo/src/App.js
@@ -1,10 +1,17 @@
+import React, { Component } from 'react';
+import InputBox from "./components/InputBox/InputBox.jsx";
+import TodosList from "./components/TodosList/TodosList.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      Hello From App
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <InputBox></InputBox>
+        <TodosList></TodosList>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ Module3_React/todo/src/components/TodosList/TodosList.jsx
@@ -0,0 +1,13 @@
+// imrc => import 
+// cc => class component
+
+import React, { Component } from 'react';
+
+class TodosList extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Hello from TodosList</h1> );
+    }
+}
+ 
+export default TodosList;
