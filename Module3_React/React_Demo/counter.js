a81540a47d50346ebdc338fe33fb1c5ff8b26ae3
sushant <sushantberiwal@gmail.com>
2021-06-13 19:10:13 +0530

    create-react-app todo

--- Module3_React/React_Demo/counter.js
+++ Module3_React/React_Demo/counter.js
@@ -1,17 +1,18 @@
 // class based stateful components
 class Counter extends React.Component {
   // 1. constructor method is the first one to fire
-  constructor() {
-    super(); // it fire React.Component's constructor
+  constructor(props) {
+    super(props); // it fire React.Component's constructor
     this.state = {
       count: 0,
+      name:props.namee
     }; // state is empty object
   }
 
   increment = () => {
     console.log("increment");
     this.setState({
-      count: this.state.count + 1,
+      count: this.state.count + 1
     }); // lifecycle events => componentDidUpdate => render
   };
 
@@ -29,13 +30,12 @@ class Counter extends React.Component {
     });
   };
 
-
   render() {
     return (
       <React.Fragment>
         <p className="badge bg-secondary d-inline-block m-2">
           {" "}
-          {this.state.count}{" "}
+          {this.state.count}
         </p>
         <div>
           <button className="btn btn-primary m-2" onClick={this.increment}>
@@ -53,4 +53,7 @@ class Counter extends React.Component {
   }
 }
 
-ReactDOM.render(<Counter name="steve"></Counter>, document.querySelector("#root"));
+ReactDOM.render(
+  <Counter namee="steve"></Counter>,
+  document.querySelector("#root")
+);
