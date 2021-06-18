0dbe479584d5adb582a5671a04f6a3687f002ebb
sushant <sushantberiwal@gmail.com>
2021-06-18 18:57:18 +0530

    movies folder created

---
+++ Module3_React/movies/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div className="App">Hello From App</div>;
+  }
+}
+
+export default App;
