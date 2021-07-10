8266cb4df0b6e882cb8fa74f619145573facb284
sushant <sushantberiwal@gmail.com>
2021-07-10 22:21:02 +0530

    login page material UI

--- Module3_React/reels/src/Components/Login.jsx
+++ Module3_React/reels/src/Components/Login.jsx
@@ -1,48 +1,138 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthProvider";
+import { Link } from "react-router-dom";
+import logo from "../logo.png";
+import {
+  TextField,
+  Grid,
+  Button,
+  Paper,
+  Card,
+  CardContent,
+  CardActions,
+  Container,
+  CardMedia,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [message , setMessage] = useState("");
-  let {login} = useContext(AuthContext);
+  const [message, setMessage] = useState("");
+  let { login } = useContext(AuthContext);
 
-  const handleLogin = async (e)=>{
+  const handleLogin = async (e) => {
     //   email , password
-    try{
-        await login(email , password);
-        props.history.push("/"); //navigate to /
+    try {
+      await login(email, password);
+      props.history.push("/"); //navigate to /
+    } catch (err) {
+      setMessage(err.message);
+      setEmail("");
+      setPassword("");
     }
-    catch(err){
-        setMessage(err.message);
-        setEmail("")
-        setPassword("");
-    }
+  };
 
-  }
+  let useStyles = makeStyles({
+    centerDivs: {
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      width: "100vw",
+    },
+    carousal: { height: "10rem", backgroundColor: "lightgray" },
+    fullWidth: {
+      width: "100%",
+    },
+    centerElements: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    mb: {
+      marginBottom: "1rem",
+    },
+    padding: {
+      paddingTop: "1rem",
+      paddingBottom: "1rem",
+    },
+    alignCenter: {
+      justifyContent: "center",
+    },
+  });
+  let classes = useStyles();
 
   return (
-    <>
-    <h1>Login Page</h1>
+    <div>
+      <Container>
+        <Grid container spacing={2}>
+          {/* Carousel */}
+          <Grid item sm={5}>
+            <Paper className={classes.carousal}>Carousel</Paper>
+          </Grid>
+          <Grid item sm={3}>
+            <Card variant="outlined" className={classes.mb}>
+              <CardMedia
+                image={logo}
+                style={{ height: "5rem", backgroundSize: "contain" }}
+              ></CardMedia>
+              <CardContent className={classes.centerElements}>
+                <TextField
+                  label="Email"
+                  type="email"
+                  variant="outlined"
+                  value={email}
+                  size="small"
+                  onChange={(e) => setEmail(e.target.value)}
+                ></TextField>
+                <TextField
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  value={password}
+                  size="small"
+                  onChange={(e) => setPassword(e.target.value)}
+                ></TextField>
+              </CardContent>
+              <CardActions>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleLogin}
+                  className={classes.fullWidth}
+                >
+                  Login
+                </Button>
+              </CardActions>
+            </Card>
+            <Card variant="outlined" className={classes.padding}>
+              <Typography style={{ textAlign: "center" }}>
+                Don't have an account ?
+                <Button variant="contained" color="primary">
+                  <Link style={{ color: "white" }} to="/signup">
+                    SignUp
+                  </Link>
+                </Button>
+              </Typography>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+      {/* <h1>Login Page</h1>
       <div>
-        <div>
-          Email
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          Password
-          <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-        </div>
+        Email
+        <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+      </div>
+      <div>
+        Password
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
       </div>
       <button onClick={handleLogin}>Login</button>
-      <h2 style={{ color: "red" }}>{message}</h2>{" "}
-    </>
+      <h2 style={{ color: "red" }}>{message}</h2>{" "} */}
+    </div>
   );
 };
 
