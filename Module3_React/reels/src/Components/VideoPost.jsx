05edfa47a621928f80a8b026f5a1d3e9f9a30069
sushant <sushantberiwal@gmail.com>
2021-07-11 21:26:06 +0530

    comments rendered from firebase

--- Module3_React/reels/src/Components/VideoPost.jsx
+++ Module3_React/reels/src/Components/VideoPost.jsx
@@ -16,29 +16,60 @@ import {
 
 const VideoPost = (props) => {
   let [user, setUser] = useState(null);
-  useEffect(() => {
+  let [comment, setComment] = useState("");
+  let [commentList, setCommentList] = useState([]);
+  // { comment , profilePhotoUrl }
+
+  useEffect(async () => {
     console.log(props);
     let uid = props.postObj.uid;
-    firebaseDB
-      .collection("users")
-      .doc(uid)
-      .get()
-      .then((doc) => {
-        let user = doc.data(); // get user who created the post
-        setUser(user);
-      });
+    let doc = await firebaseDB.collection("users").doc(uid).get();
+    let user = doc.data();
+    let commentList = props.postObj.comments;
+    // {uid , comment} , {uid , comment} , {uid , comment};
+    let updatedCommentList=[];
+    
+    for(let i=0 ; i<commentList.length ; i++){
+      let commentObj = commentList[i];
+      let doc = await firebaseDB.collection("users").doc(commentObj.uid).get();
+      let commentUserPic = doc.data().profileImageUrl;
+      updatedCommentList.push({ profilePic: commentUserPic, comment: commentObj.comment });
+    }
+    
+    console.log(updatedCommentList);
+    setUser(user);
+    setCommentList(updatedCommentList);
   }, []); //comp did Mount
 
   return (
-      <Container>
-        <Card style={{ height: "600px", width: "300px" }}>
-          <Avatar src={user ? user.profileImageUrl : ""}></Avatar>
-          <Typography variant="span">{user ? user.username : ""}</Typography>
-          <div className="video-container">
-            <Video src={props.postObj.videoLink}></Video>
-          </div>
-        </Card>
-      </Container>
+    <Container>
+      <Card style={{ height: "600px", width: "300px" }}>
+        <Avatar src={user ? user.profileImageUrl : ""}></Avatar>
+        <Typography variant="span">{user ? user.username : ""}</Typography>
+        <div className="video-container">
+          <Video src={props.postObj.videoLink}></Video>
+        </div>
+        <Typography variant="p">Comments</Typography>
+
+        <TextField
+          variant="outlined"
+          label="Add a comment"
+          size="small"
+        ></TextField>
+        <Button variant="contained" color="secondary">
+          Post
+        </Button>
+
+        {commentList.map((commentObj) => {
+          return (
+            <>
+              <Avatar src={commentObj.profilePic}></Avatar>
+              <Typography variant="p">{commentObj.comment}</Typography>
+            </>
+          );
+        })}
+      </Card>
+    </Container>
   );
 };
 
